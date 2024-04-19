82970b6097f94aac1d028999a7e75f3c0bf2ffe2
bereket <beki4work@gmail.com>
2024-04-19 16:01:36 +0300

    provider_lab_practice

---
+++ provider_state_management/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class CounterModel with ChangeNotifier {
+  int count = 0;
+
+  increment() {
+    count++;
+    notifyListeners();
+  }
+
+  decrement() {
+    count--;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    CounterModel counter = Provider.of<CounterModel>(context, listen: false);
+
+    return Scaffold(
+      body: Consumer<CounterModel>(builder: (context, notifier, child) {
+        return Center(
+          child: Text(
+            "${notifier.count}",
+          ),
+        );
+      }),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              counter.increment();
+            },
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              counter.decrement();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
